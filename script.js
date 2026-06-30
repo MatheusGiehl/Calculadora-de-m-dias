435cf191e46a543e8b77ca72743fa54f198b3645
Matheus Giehl <giehlmatheus96@gmail.com>
2026-06-29 21:56:23 -0300

    testes problemas console

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-let nota1 = Number(document.getElementById("nota1").value);
-let nota2 = Number(document.getElementById("nota2").value);
-let nota3 = Number(document.getElementById("nota3").value);
-
 function calcularMedia() {
+    let nota1 = Number(document.getElementById("nota1").value);
+    let nota2 = Number(document.getElementById("nota2").value);
+    let nota3 = Number(document.getElementById("nota3").value);
+
     let media = (nota1 + nota2 + nota3) / 3;
-    document.getElementById("resultado").innerHTML = "<p>A média é: " + media.toFixed(2) + "</p>";
+    document.getElementById("resultado").innerHTML = "A média é: " + media.toFixed(2);
 }
 
 function resultado() {
+    let nota1 = Number(document.getElementById("nota1").value);
+    let nota2 = Number(document.getElementById("nota2").value);
+    let nota3 = Number(document.getElementById("nota3").value);
+
+    let media = (nota1 + nota2 + nota3) / 3;
+
     if (media >= 7) {
         document.getElementById("resultado").innerHTML = "Aprovado";
     } else if (media >= 5) {
